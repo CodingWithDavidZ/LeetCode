360f6abe3ca24cd80183cbc6d757a42178a2f51a
David Zombar <82234401+CodingWithDavidZ@users.noreply.github.com>
2023-11-01 14:39:11 -0600

    changed space complexity from linear to constant

--- 1768 Merge Strings Alternately/MergeStringsAlternately.js
+++ 1768 Merge Strings Alternately/MergeStringsAlternately.js
@@ -19,4 +19,4 @@ function mergeAlternately(word1, word2) {
 console.log(mergeAlternately("Twitch", "Streaming"))
 
 //Time Complexity: O(n) - linear
-//Space Complexity: O(n) - linear
+//Space Complexity: O(1) - constant
